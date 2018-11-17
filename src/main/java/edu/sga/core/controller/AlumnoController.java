5b8b860b35f75f91611472ab4c3ecc4a596af5c9
Edwin Tumax <edwintumax@gmail.com>
2018-11-17 17:13:37 -0600

    Agregando CRUD

--- src/main/java/edu/sga/core/controller/AlumnoController.java
+++ src/main/java/edu/sga/core/controller/AlumnoController.java
@@ -3,13 +3,17 @@ package edu.sga.core.controller;
 import edu.sga.core.bs.dao.AlumnoRepository;
 import edu.sga.core.eis.bo.Alumno;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.text.html.parser.Entity;
+import javax.xml.ws.Response;
+import java.lang.invoke.MethodType;
 
 @RestController
-@RequestMapping(value="/api/v1/alumno")
+@RequestMapping(value="/api/v1/alumno",produces = MediaType.APPLICATION_JSON_VALUE)
 public class AlumnoController {
     @Autowired
     private AlumnoRepository alumnoRepository;
@@ -17,5 +21,38 @@ public class AlumnoController {
     @RequestMapping(method = RequestMethod.GET)
     public Iterable<Alumno> getAll(){
         return  alumnoRepository.findAll();
+    }
+    @CrossOrigin(origins = {"http://localhost:9494","http://localhost:9300"})
+    @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+    public ResponseEntity<Alumno> update(@PathVariable("id") Long id,
+                                         @RequestBody Alumno alumno){
+        if(id == null || id < 0){
+            return new ResponseEntity("{\"message\": \"Is required id\" }"
+                    ,HttpStatus.CONFLICT);
+        }
+        if(alumnoRepository.findOne(id) == null){
+            return new ResponseEntity("{\"message\" : \"no exist\"}"
+                    ,HttpStatus.NO_CONTENT);
+        }
+        Alumno currenAlumno = alumnoRepository.findOne(id);
+        currenAlumno.setApellidos(alumno.getApellidos());
+        currenAlumno.setNombres(alumno.getNombres());
+        currenAlumno.setFechaNacimiento(alumno.getFechaNacimiento());
+        alumnoRepository.save(currenAlumno);
+        return new ResponseEntity<Alumno>(currenAlumno,HttpStatus.OK);
+    }
+    @CrossOrigin(origins = {"http://localhost:9494","http://localhost:9300"})
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    public ResponseEntity<?> delete(@PathVariable("id") Long id){
+        if(id == null || id < 0){
+            return new ResponseEntity("{\"message\": \"Is required id\" }"
+                    ,HttpStatus.CONFLICT);
+        }
+        if(alumnoRepository.findOne(id) == null){
+            return new ResponseEntity("{\"message\" : \"no exist\"}"
+                    ,HttpStatus.NO_CONTENT);
+        }
+        alumnoRepository.delete(id);
+        return  new ResponseEntity("{\"message\" : \"delete alumno\"}",HttpStatus.OK);
     }
 }
